3ac8480e43068815579fec18db1c3bd6a71f712b
zhtsu <zhtsu@qq.com>
2022-11-02 08:50:07 +0800

    add 3rdparty done

--- src/Alice/UI/ImGuiLayer.hpp
+++ src/Alice/UI/ImGuiLayer.hpp
@@ -17,17 +17,6 @@ public:
     void OnAttach();
     void OnDetach();
     void OnUpdate();
-    void OnEvent(Event& event);
-
-private:
-    bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& event);
-    bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& event);
-    bool OnMouseMovedEvent(MouseMovedEvent& event);
-    bool OnMouseScrolledEvent(MouseScrolledEvent& event);
-    bool OnKeyPressedEvent(KeyPressedEvent& event);
-    bool OnKeyReleasedEvent(KeyReleasedEvent& event);
-    bool OnKeyTypedEvent(KeyTypedEvent& event);
-    bool OnWindowResizedEvent(WindowResizeEvent& event);
 
 private:
     float m_time;
