fc49d56509bb3e209d0bccc743dd3443a95ce39d
zhtsu <zhtsu@qq.com>
2022-12-15 18:09:37 +0800

    Add folder resources

--- src/Editor/Panel/ContentBrowserPanel.cpp
+++ src/Editor/Panel/ContentBrowserPanel.cpp
@@ -1,6 +1,7 @@
 #include "ContentBrowserPanel.hpp"
 
 #include <imgui.h>
+#include "Alice/Renderer/Texture.hpp"
 
 namespace Alice
 {
@@ -10,7 +11,8 @@ static const std::filesystem::path k_assets_path = "assets";
 ContentBrowserPanel::ContentBrowserPanel()
     : m_current_directory(k_assets_path)
 {
-
+    m_folder_icon = Texture2D::Create("resources/icons/folder.png");
+    m_txt_icon = Texture2D::Create("resources/icons/txt.png");
 }
 
 void ContentBrowserPanel::OnImGuiRender()
@@ -25,25 +27,37 @@ void ContentBrowserPanel::OnImGuiRender()
         }
     }
 
+    static float padding = 16.0f;
+    static float thumbnail_size = 128.0f;
+    float cell_size = thumbnail_size + padding;
+
+    float panel_width = ImGui::GetContentRegionAvail().x;
+    int column_count = (int)(panel_width / cell_size);
+    if (column_count < 1)
+        column_count = 1;
+
+    ImGui::Columns(column_count, 0, false);
+
     for (auto& iter : std::filesystem::directory_iterator(m_current_directory))
     {
         const auto& path = iter.path();
         auto relative_path = std::filesystem::relative(path, k_assets_path);
         std::string filename_string = relative_path.filename().string();
-        if (iter.is_directory())
-        {
-            if (ImGui::Button(filename_string.c_str()))
-            {
-                m_current_directory /= path.filename();
-            }
-        }
-        else
-        {
-            if (ImGui::Button(filename_string.c_str()))
-            {
 
-            }
+        Ref<Texture2D> icon = iter.is_directory() ? m_folder_icon : m_txt_icon;
+        ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0, 0, 0, 0));
+        ImGui::ImageButton((ImTextureID)icon->GetRendererID(), { thumbnail_size, thumbnail_size }, { 0, 1 }, { 1, 0 });
+        ImGui::PopStyleColor();
+
+        if (ImGui::IsItemHovered() && ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left))
+        {
+            if (iter.is_directory())
+                m_current_directory /= path.filename();
         }
+
+        ImGui::TextWrapped(filename_string.c_str());
+
+        ImGui::NextColumn();
     }
 
     ImGui::End();
--- src/Editor/Panel/ContentBrowserPanel.hpp
+++ src/Editor/Panel/ContentBrowserPanel.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
 #include <filesystem>
+#include "Alice/Core/Base.hpp"
 
 namespace Alice
 {
+
+class Texture2D;
 
 class ContentBrowserPanel
 {
@@ -14,6 +17,11 @@ public:
 
 private:
     std::filesystem::path m_current_directory;
+    Ref<Texture2D> m_folder_icon;
+    Ref<Texture2D> m_txt_icon;
+    Ref<Texture2D> m_back_icon;
+    // Ref<Texture2D> m_source_icon;
+    // Ref<Texture2D> m_image_icon;
 };
 
 } // namespace Alice
