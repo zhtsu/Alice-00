96ddbbcc4b4e2841ebcdd306e7bc9f6423a38a51
zhtsu <zhtsu@qq.com>
2022-12-15 19:15:04 +0800

    Update color of icons to white from black

--- src/Editor/Panel/ContentBrowserPanel.cpp
+++ src/Editor/Panel/ContentBrowserPanel.cpp
@@ -13,6 +13,7 @@ ContentBrowserPanel::ContentBrowserPanel()
 {
     m_folder_icon = Texture2D::Create("resources/icons/folder.png");
     m_txt_icon = Texture2D::Create("resources/icons/txt.png");
+    m_back_icon = Texture2D::Create("resources/icons/back.png");
 }
 
 void ContentBrowserPanel::OnImGuiRender()
@@ -21,14 +22,25 @@ void ContentBrowserPanel::OnImGuiRender()
 
     if (m_current_directory != std::filesystem::path(k_assets_path))
     {
-        if (ImGui::Button("<-"))
+        bool is_back = ImGui::ImageButton(
+            reinterpret_cast<ImTextureID>(m_back_icon->GetRendererID()),
+            { 10, 10 }, { 0, 1 }, { 1, 0 });
+        if (is_back)
         {
             m_current_directory = m_current_directory.parent_path();
         }
     }
+    else
+    {
+        ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0, 0, 0, 0));
+        ImGui::ImageButton(
+            reinterpret_cast<ImTextureID>(m_back_icon->GetRendererID()),
+            { 10, 10 }, { 0, 1 }, { 1, 0 });
+        ImGui::PopStyleColor();
+    }
 
     static float padding = 16.0f;
-    static float thumbnail_size = 128.0f;
+    static float thumbnail_size = 64.0f;
     float cell_size = thumbnail_size + padding;
 
     float panel_width = ImGui::GetContentRegionAvail().x;
@@ -46,7 +58,8 @@ void ContentBrowserPanel::OnImGuiRender()
 
         Ref<Texture2D> icon = iter.is_directory() ? m_folder_icon : m_txt_icon;
         ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0, 0, 0, 0));
-        ImGui::ImageButton((ImTextureID)icon->GetRendererID(), { thumbnail_size, thumbnail_size }, { 0, 1 }, { 1, 0 });
+        ImGui::ImageButton(reinterpret_cast<ImTextureID>(icon->GetRendererID()),
+            { thumbnail_size, thumbnail_size }, { 0, 1 }, { 1, 0 });
         ImGui::PopStyleColor();
 
         if (ImGui::IsItemHovered() && ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left))
